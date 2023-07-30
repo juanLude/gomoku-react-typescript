45af2ac060cd599b45f24b3cfc58e73b648c93b6
Juan Ludevid <jmludevid@hotmail.com>
2023-07-30 13:58:43 +1000

    create handle login function

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,33 @@
 import { useState } from "react";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import users from "../data/users.json";
+import "./Login.css";
+import { log } from "console";
 
 export default function Login() {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
   console.log(username);
+
+  const handleLogin = () => {
+    const user = users.find(
+      (u) => u.username === username && u.password === password
+    );
+    if (!user) {
+      console.log("invalid username or password");
+    } else {
+      console.log("user logged in");
+    }
+  };
   return (
-    <form>
+    <form
+      className="container"
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleLogin();
+      }}
+    >
       <Input
         name="username"
         placeholder="Username"
